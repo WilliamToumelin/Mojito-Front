468fbb7432400af25fd9399b0c17275f3963ff1b
TommyDron <tommy.dron@oclock.school>
2023-08-22 09:47:46 +0200

    hotfix ConnectModal-Register

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,15 +3,18 @@ import { Link } from 'react-router-dom';
 
 const Footer: React.FC = () => {
   return (
-    <div className="h-[3em] bg-black flex justify-evenly text-white">
+    <div className="h-[3em] bg-black flex justify-evenly items-center text-white">
       <Link to="/a-propos-de-nous" className="text-white">
         A propos de nous
       </Link>
+      <Link
+        to="/proposition-cocktail"
+        className="text-white rounded-full bg-gradient-to-r from-purple-700 via-pink-500 to-orange-500 p-2 font-bold"
+      >
+        Propose un cocktail !
+      </Link>
       <Link to="/mentions-legales" className="text-white">
         Mentions Légales
-      </Link>
-      <Link to="/proposition-cocktail" className="text-white">
-        Propose un de tes cocktails !
       </Link>
     </div>
   );
