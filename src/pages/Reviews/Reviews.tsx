ade384a83f123d4dcd86150b236b75f7e1a906b8
WilliamToumelin <william.toumelin@oclock.school>
2023-09-08 18:32:21 +0200

    ma

--- src/pages/Reviews/Reviews.tsx
+++ src/pages/Reviews/Reviews.tsx
@@ -36,7 +36,7 @@ const Reviews: React.FC = () => {
   useEffect(() => {
     fetch(`${apiHostName}/api/cocktails/${selectedCocktailId}/comments`)
       .then((response) => response.json())
-      .then((data) => {
+      .then((data: Cocktails) => {
         setCocktailData(data);
       })
       .catch((err) => console.error(err));
@@ -46,81 +46,91 @@ const Reviews: React.FC = () => {
     setDisplayModal((prevstate) => !prevstate);
   }, []);
 
+  if (!cocktailData) {
+    // TEMPORAIRE !!!!!! a rajouter un loader , parce pour l'instant il n'y a que une fenetre noir si pas de cocktail chargé encore ...
+    return (
+      <div className="bg-light-brown flex justify-center items-center flex-1 h-[75vh]">
+        <div
+          style={{
+            boxShadow: '#132226 0px 1px 22px',
+          }}
+          className="relative  w-4/5 xl:w-4/6 h-4/5 flex flex-col overflow-y-auto shadow-light-gray shadow-xl rounded-2xl pb-3 bg-dark-gray"
+        />
+      </div>
+    );
+  }
+
   return (
     <div className="bg-light-brown flex justify-center items-center flex-1 h-[75vh]">
-      {cocktailData ? (
-        cocktailData.comments.length > 0 && (
-          <div
-            style={{
-              boxShadow: '#132226 0px 1px 22px',
-            }}
-            className="relative  w-4/5 xl:w-4/6 h-4/5 flex flex-col overflow-y-auto shadow-light-gray shadow-xl rounded-2xl pb-3 bg-dark-gray"
-          >
-            <div className="relative w-full p-3 mdflex flex-col flex-start animate-fade-in-down">
-              <div className="w-full flex flex-wrap justify-around items-center">
-                <h1 className="text-dark-brown text-xl md:text-3xl">
-                  {cocktailData.name}
-                </h1>
-                <div className={`${!isLoggedIn ? 'hidden' : ''}`}>
-                  <div className="">
-                    <SquaredButton
-                      name="Commente ! :)"
-                      onClick={handleToggleModal}
-                      height={3}
-                      width={7}
-                      type="button"
-                    />
-                  </div>
-                  <CommentModal
-                    displayModal={displayModal}
-                    handleToggleModal={handleToggleModal}
+      {cocktailData.comments.length >= 0 && (
+        <div
+          style={{
+            boxShadow: '#132226 0px 1px 22px',
+          }}
+          className="relative  w-4/5 xl:w-4/6 h-4/5 flex flex-col overflow-y-auto shadow-light-gray shadow-xl rounded-2xl pb-3 bg-dark-gray"
+        >
+          <div className="relative w-full p-3 mdflex flex-col flex-start animate-fade-in-down">
+            <div className="w-full flex flex-wrap justify-around items-center">
+              <h1 className="text-dark-brown text-xl md:text-3xl">
+                {cocktailData.name}
+              </h1>
+              <div className={`${!isLoggedIn ? 'hidden' : ''}`}>
+                <div className="">
+                  <SquaredButton
+                    name="Commente ! :)"
+                    onClick={handleToggleModal}
+                    height={3}
+                    width={7}
+                    type="button"
                   />
                 </div>
+                <CommentModal
+                  displayModal={displayModal}
+                  handleToggleModal={handleToggleModal}
+                />
               </div>
             </div>
-            <div className="border-2 border-light-brown animate-fade-in-down" />
-            <div className="w-full h-full overflow-y-auto p-6 pt-0 flex flex-col items-center animate-fade-in-down">
-              {cocktailData.comments.map((data) => (
-                <article
-                  key={data.id}
-                  className="w-[80%] sm:mx-auto mt-4 md:mt-14 sm:p-4 rounded-lg flex items-center justify-center text-center"
-                >
-                  <div className="text-center">
-                    <svg
-                      className="w-10 h-10 mx-auto mb-3 text-gray-400 dark:text-gray-600"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="currentColor"
-                      viewBox="0 0 18 14"
-                    >
-                      <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z" />
-                    </svg>
-                    <blockquote>
-                      <p className="text-lg md:text-2xl italic font-medium text-light-brown">
-                        {data.content}
-                      </p>
-                    </blockquote>
-                    <div className=" mt-6 space-x-3">
-                      <div
-                        className="divide-x-2 divide-gray-500 dark:divide-gray-700
+          </div>
+          <div className="border-2 border-light-brown animate-fade-in-down" />
+          <div className="w-full h-full overflow-y-auto p-6 pt-0 flex flex-col items-center animate-fade-in-down">
+            {cocktailData.comments.map((data) => (
+              <article
+                key={data.id}
+                className="w-[80%] sm:mx-auto mt-4 md:mt-14 sm:p-4 rounded-lg flex items-center justify-center text-center"
+              >
+                <div className="text-center">
+                  <svg
+                    className="w-10 h-10 mx-auto mb-3 text-gray-400 dark:text-gray-600"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 18 14"
+                  >
+                    <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z" />
+                  </svg>
+                  <blockquote>
+                    <p className="text-lg md:text-2xl italic font-medium text-light-brown">
+                      {data.content}
+                    </p>
+                  </blockquote>
+                  <div className=" mt-6 space-x-3">
+                    <div
+                      className="divide-x-2 divide-gray-500 dark:divide-gray-700
                     "
-                      >
-                        <span className="pr-3 font-medium text-dark-brown">
-                          {data.user.pseudonym}
-                        </span>
-                        <span className="pl-3 text-sm text-dark-brown">
-                          {formatDate(data.posted_at)}
-                        </span>
-                      </div>
+                    >
+                      <span className="pr-3 font-medium text-dark-brown">
+                        {data.user.pseudonym}
+                      </span>
+                      <span className="pl-3 text-sm text-dark-brown">
+                        {formatDate(data.posted_at)}
+                      </span>
                     </div>
                   </div>
-                </article>
-              ))}
-            </div>
+                </div>
+              </article>
+            ))}
           </div>
-        )
-      ) : (
-        <p>pas de commentaire </p>
+        </div>
       )}
     </div>
   );
