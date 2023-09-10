f0ab424e8f1fbb413e9e19ce6c80a1c478113b9d
WilliamToumelin <william.toumelin@oclock.school>
2023-09-10 17:39:58 +0200

    cookie consent modal enhancement

--- src/components/Modals/CookieConsentModal.tsx
+++ src/components/Modals/CookieConsentModal.tsx
@@ -14,46 +14,34 @@ const CookieConsentModal = () => {
 
   const handleAccept = () => {
     // L'utilisateur a accepté les cookies, vous pouvez stocker un cookie de consentement ici
-    Cookies.set('cookieConsent', 'true', { expires: 365 }); // Expire dans 1 an
+    Cookies.set('cookieConsent', 'true', { expires: 1 }); // Expire dans 1 jour
     setConsent(true);
   };
 
-  const handleReject = () => {
-    // L'utilisateur a refusé les cookies, vous pouvez gérer cela selon vos besoins
-    setConsent(false);
-    Cookies.set('cookieConsent', 'false', { expires: 1 }); // Expire dans 1 jour
-  };
-
-  if (consent === true || false || null) {
+  if (consent === null || consent === false) {
     // Rendre le composant si l'état est null (affichage initial)
     return (
       <div className="fixed z-50 bottom-20 right-30">
         <div className="bg-dark-gray p-4 h-[30vh] w-[40vw] rounded border-4 border-light-gray">
-          <div className="flex flex-col h-[50%] w-full">
+          <div className="flex flex-col h-full justify-center">
             <h3 className="text-xl font-medium text-dark-brown mb-3 flex items-center">
               Enjoy the experience with some{'   '}
               <GiCookie className="text-[2rem]" />
               <MdCookie className="text-[2rem]" />
             </h3>
             <p className="text-dark-brown mb-3">
-              Nous utilisons des cookies pour améliorer votre expérience sur
-              notre site.
+              Nous utilisons des cookies strictement nécessaires pour le bon
+              fonctionnement de notre site, tels que le stockage de votre token
+              JWT et de certaines informations de base que vous nous fournissez
+              a votre inscritption sur notre site. Cependant, par souci de
+              transparence, nous vous informons de leur utilisation.
             </p>
-          </div>
-          <div className="flex flex-col h-[50%] justify-around">
             <button
               type="button"
               className="rounded bg-dark-brown text-white px-4 py-2"
               onClick={handleAccept}
             >
-              Accepter
-            </button>
-            <button
-              type="button"
-              className="rounded bg-red-900 text-white px-4 py-2"
-              onClick={handleReject}
-            >
-              Refuser
+              OK, j'ai compris
             </button>
           </div>
         </div>
@@ -61,7 +49,7 @@ const CookieConsentModal = () => {
     );
   }
 
-  return null; // Au cas où quelque chose ne fonctionne pas
+  return null; // Le consentement est déjà donné, donc ne rien afficher
 };
 
 export default CookieConsentModal;
